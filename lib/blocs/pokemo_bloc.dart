ed5f12ab1b40e1e518b7f619337efc223e42e2b1
kiril <kirilnoskov402@gmail.com>
2022-02-23 08:00:49 +0300

    firt method in pokeBloc done

--- lib/blocs/pokemo_bloc.dart
+++ lib/blocs/pokemo_bloc.dart
@@ -1,17 +1,48 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:math';
 
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:http/http.dart' as http;
 import 'package:pokemon/models/card_model.dart';
 import 'package:pokemon/models/pokemon_model.dart';
 
 part 'pokemo_event.dart';
+
 part 'pokemo_state.dart';
 
 class PokemonBloc extends Bloc<PokemonEvent, PokemonState> {
   PokemonBloc() : super(PokemonState.initial()) {
-    on<PokemonEvent>((event, emit) {
-
+    on<GetRandomPokemonData>((event, emit) async {
+      int pokeNumber = 5;
+      List<CardModel> tempList = [];
+      emit(state.copyWith(isRequestError: false));
+      for (int index = 1; index < pokeNumber + 1; index++) {
+        int pokeIndex;
+        Random rnd;
+        int min = 1;
+        int max = 893;
+        rnd = new Random();
+        pokeIndex = min + rnd.nextInt(max - min);
+        try {
+          emit(state.copyWith(isLoading: true));
+          Uri url = Uri.parse('https://pokeapi.co/api/v2/pokemon/$pokeIndex');
+          final response = await http.get(url);
+          final responseData =
+              json.decode(response.body) as Map<String, dynamic>;
+          tempList.add(CardModel.fromJson(responseData));
+        } catch (e) {
+          throw (e);
+        }
+      }
+      if (tempList.length == pokeNumber) {
+        emit(state.copyWith(
+          pokeList: tempList,
+          isLoading: false,
+        ));
+        inspect(state.pokeList);
+      }
     });
   }
 }
--- lib/blocs/pokemo_state.dart
+++ lib/blocs/pokemo_state.dart
@@ -1,33 +1,38 @@
 part of 'pokemo_bloc.dart';
 
- class PokemonState {
+class PokemonState {
+  final bool isLoading;
+  final bool isRequestError;
+  final List<Pokemon> descList;
+  final List<CardModel> pokeList;
 
-final bool isLoading;
-final bool isRequesError;
-final List<Pokemon> descList;
-final List<CardModel> pokeList;
+  PokemonState({
+    required this.isLoading,
+    required this.isRequestError,
+    required this.descList,
+    required this.pokeList,
+  });
 
+  factory PokemonState.initial() {
+    return PokemonState(
+      isLoading: false,
+      isRequestError: false,
+      descList: [],
+      pokeList: [],
+    );
+  }
 
-const PokemonState({
- required this.isLoading,
- required this.isRequesError,
- required this.descList,
- required this.pokeList,
- });
-
-factory PokemonState.initial() {
- return PokemonState(
-  isLoading: false,
-  isRequesError: false,
-  descList: [],
-  pokeList:[],
- );
+  PokemonState copyWith({
+    bool? isLoading,
+    bool? isRequestError,
+    List<CardModel>? pokeList,
+    List<Pokemon>? descList,
+  }) {
+    return PokemonState(
+      isLoading: isLoading ?? this.isLoading,
+      isRequestError: isRequestError ?? this.isRequestError,
+      pokeList: pokeList ?? this.pokeList,
+      descList: descList ?? this.descList,
+    );
+  }
 }
-
-
-
-
-
- }
-
-
