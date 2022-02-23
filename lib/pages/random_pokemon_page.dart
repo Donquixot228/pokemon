2f197366a5e23af0ecce4ed6dcbcba02e6b25e6f
kiril <kirilnoskov402@gmail.com>
2022-02-23 07:22:59 +0300

    structure

---
+++ lib/pages/random_pokemon_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class RandomPokemonPage extends StatelessWidget {
+  const RandomPokemonPage();
+  static const String routeName = '/randomPokemonPage';
+
+  static Widget create() {
+    return const RandomPokemonPage();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/pages/search_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatelessWidget {
+  const SearchPage();
+  static const String routeName = '/searchPage';
+
+  static Widget create() {
+    return const SearchPage();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
