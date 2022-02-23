3cc26e44a43090e52175e001e33b5dd4434b0506
kiril <kirilnoskov402@gmail.com>
2022-02-23 07:15:14 +0300

    Initial commit

---
+++ lib/resources/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors._();
+
+  static const Color purpleColor = Color(0xFF8077E4);
+  static const Color audioFilesColor = Color(0xFF5E77CE);
+  static const Color collectionColor = Color(0xFF71A59F);
+  static const Color whiteColor = Color(0xFFF6F6F6);
+  static const Color sendColor = Color(0xFFF1B488);
+  static const Color textColor = Color(0xFF3A3A55);
+}
